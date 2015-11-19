98cc5bbd9cbe1483b57b20cafd3575081012d49a
congnd91 <congnd91@gmail.com>
2015-11-19 14:49:08 +0700

    udpate code

--- TravisSample/Calculator.cs
+++ TravisSample/Calculator.cs
@@ -10,7 +10,7 @@ namespace TravisSample
     {
         public int Add(int x, int y)
         {
-            return x + y;
+            return x + y 
         }
 
         public int Subtract(int x, int y)
